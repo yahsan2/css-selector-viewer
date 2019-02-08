343db6457110f5ea440189943f91c2a010a37223
Ryosuke Miyamoto <yahsan2@gmail.com>
2019-02-08 22:18:35 +0800

    Add footer and pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,13 +21,28 @@
       </v-list>
     </v-navigation-drawer>
     <v-footer
+      class="footer"
       :fixed="fixed"
       app
     >
-      <span>&copy; 2019</span>
+      <n-link class="mr-2" to="/">トップ</n-link>
+      <n-link class="mr-2" to="/privacy">プライバシーポリシー</n-link>
+      <n-link class="mr-2" to="/term">利用規約</n-link>
+      <span>/ Created by <a href="https://twitter.com/yahsan2/">@yahsan2</a> &copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
+
+<style scoped>
+.footer{
+  padding: 10px 20px;
+  text-align: center;
+}
+.footer span, a  {
+  color: #999;
+}
+</style>
+
 
 <script>
 export default {
